4e7df814ac0d60ee084cc5a51a127825ebdcc706
12moi <moi.shadrack@student.moringaschool.com>
2022-02-05 21:14:42 +0300

    add form component to collect people's quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuotedetailComponent } from './quotedetail/quotedetail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuotedetailComponent
+    QuotedetailComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
