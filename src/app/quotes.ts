a2ff4511dd163297341fca28b484848afbdce32e
12moi <moi.shadrack@student.moringaschool.com>
2022-02-06 12:16:35 +0300

    add quotes array

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,7 @@
 export class Quotes {
-    public showDescription: boolean;
-    constructor(public id: number,public name: string,public likes: number, public quote:string, public author:string, public dislike:number, public datePosted: Date){
-      this.showDescription=false;
+    public showDescription!: boolean;
+  showInfo: boolean;
+    constructor(public id:number,public name:string, public title:string,public quote:string, public author:string,public likes:number, public dislikes:number, public datePosted: Date){
+      this.showInfo=false;
     }
 }
