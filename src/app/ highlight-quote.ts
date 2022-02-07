8b36277faee00fabe3d540bae24e3750a46fc748
12moi <moi.shadrack@student.moringaschool.com>
2022-02-07 12:44:26 +0300

    add updates

--- src/app/ highlight-quote.ts
+++ src/app/ highlight-quote.ts
@@ -4,19 +4,6 @@ import { Directive, HostListener } from '@angular/core';
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-  [x: string]: any;
-
-
-  constructor() { }
-  @HostListener("click") onClicks(){
-    this.textDeco("green")
-  }
-
-  @HostListener("dblclick") onDoubleClicks(){
-    this.textDeco("None")
-  }
-  private textDeco(action:string){
-    this['elem'].nativeElement.style.color=action;
-  }
+  
 
 }
